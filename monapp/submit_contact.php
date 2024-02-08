1dd80905239b7deb670d635751c17e53a0868838
Aliona <aliona-balti@outlook.fr>
2024-02-08 14:17:34 +0100

    ajout des commentaires et ajout la fonction uniqid() pour générer un nom de fichier unique et la fonction strtolower() pour convertir l'extension du fichier en minuscules

--- monapp/submit_contact.php
+++ monapp/submit_contact.php
@@ -18,17 +18,25 @@ if (
 }
 
 $isFileUploaded = false;
-//Testons si un fichier a été envoyé et s'il n'y a pas d'erreur
+//Testons si un fichier a été envoyé avec "isset" et s'il n'y a pas d'erreur 
 if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] === 0) {
-    //Testons si le fichier n'est pas trop gros
+    //---Vérifiez la taille du fichier----//
+    //On vérifie si la taille du fichier est inférieure ou égale à 1 Mo (1 Mo = 1000000 octets)
     if ($_FILES['screenshot']['size'] <= 1000000) {
         echo "L'envoi n'a pas pu être effectué, erreur ou image trop volumineuse";
         return;
     }
-    //Testons si l'extension , n'est pas autorisée
+
+    //---Testons si l'extension , n'est pas autorisée---//
+
+    //On récupère l'extension du fichier envoyé
+    //La fonction pathinfo renvoie un tableau (array) contenant entre autres l'extension du fichier dans  $fileInfo['extension']  .
     $fileInfo = pathinfo($_FILES['screenshot']['name']);
-    $extension = $fileInfo['extension'];
+    //j'ai utilisé la fonction "strtolower()" pour convertir l'extension du fichier en minuscules
+    //On stocke ça dans une variable  $extension 
+    $extension = strtolower($fileInfo['extension']);
     $allowedExtensions = ['png', 'jpg', 'jpeg', 'gif'];
+    //Une fois l'extension récupérée, on peut la comparer à un tableau d'extensions autorisées, et vérifier si l'extension récupérée fait bien partie des extensions autorisées à l'aide de la fonction in_array() .
     if (!in_array($extension, $allowedExtensions)) {
         echo "L'envoi n'a pas pu être effectué,l'extension {$extension} n'est pas autorisée";
         return;
@@ -36,16 +44,30 @@ if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] === 0) {
 
     // On verifier si le dossier uploads est manquant
     $path = __DIR__ . '/uploads';
+    //Pour vérifier si notre dossier uploads existe, nous allons utiliser la fonction  is_dir()
     if (!is_dir($path)) {
         echo "L'envoi n'a pas pu être effectué, le dossier uploads est manquant";
         return;
     }
 
-    // On peut valider le fichier et le stocker dans un dossier
-    //On déplace le fichier de la mémoire temporaire vers un dossier sur le serveur
-    move_uploaded_file($_FILES['screenshot']['tmp_name'], $path . basename($_FILES['screenshot']['name']));
-    $isFileUploaded = true;
+    // Générons un nom de fichier unique pour éviter l'écrasement
+    //On peut générer un nom de fichier unique pour éviter l'écrasement de fichiers en utilisant la fonction  uniqid()  qui génère un identifiant unique basé sur la date et l'heure actuelles.
+    $filename = uniqid() . '.' . $extension;
+    // On peut valider le fichier et le stocker définitivement. Si tout est bon, on accepte le fichier en appelant la fonction move_uploaded_file() et on lui 2 paramètres : le fichier temporaire et le chemin de destination, pour déplacer le fichier temporaire vers le dossier sur le server uploads.
+    move_uploaded_file($_FILES['screenshot']['tmp_name'], $path . $filename);
+
+    echo "Fichier envoyé avec succès sous le nom : $filename";
 }
+
+
+//Lorsque vous mettrez le script sur Internet à l'aide d'un logiciel FTP, vérifiez que le dossier « Uploads » sur le serveur existe, et qu'il a les droits d'écriture. Pour ce faire, sous FileZilla par exemple, faites un clic droit sur le dossier et choisissez « Attributs du fichier ».
+//Cela vous permettra d'éditer les droits du dossier (on parle de CHMOD). Mettez les droits à 733, ainsi PHP pourra placer les fichiers téléversés dans ce dossier.
+
+
+//Comme  $_FILES['screenshot']['name']  contient le chemin entier vers le fichier d'origine (  C:\dossier\fichier.png  , par exemple), il nous faudra extraire le nom du fichier.
+//On peut utiliser pour cela la fonction  basename  qui renverra juste « fichier.png ».
+// move_uploaded_file($_FILES['screenshot']['tmp_name'], $path . basename($_FILES['screenshot']['name']));
+// $isFileUploaded = true;
 
 ?>
 
