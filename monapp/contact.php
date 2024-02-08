498da77508b50a4b0054bc3dce23c1cf200b8be4
Aliona <aliona-balti@outlook.fr>
2024-02-08 12:42:59 +0100

    Paramétrager le formulaire d'envoi de fichier

--- monapp/contact.php
+++ monapp/contact.php
@@ -19,16 +19,25 @@
         <!--Il y a deux attributs très importants à connaître pour la balise <form>  :
             La méthode :  method  
             Et la cible :  action (la page qui recevra les données du formulaire, et qui sera chargée de les traiter.) -->
-        <form action="submit_contact.php" method="Post">
+        <!-- enctype="multipart/form-data" est utilisé pour envoyer des fichiers-->
+        <form action="submit_contact.php" method="Post" enctype="multipart/form-data">
+            <!-- Ajout champ email ! -->
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
                 <div id="email-help" class="form-text">Nous ne revendrons pas votre email.</div>
             </div>
+            <!-- Ajout champ message ! -->
             <div class="mb-3">
                 <label for="message" class="form-label">Votre message</label>
                 <textarea class="form-control" placeholder="Exprimez vous" id="message" name="message"></textarea>
             </div>
+            <!-- Ajout champ d'upload ! -->
+            <div class="mb-3">
+                <label for="screenshot" class="form-label">Votre capture d'écran</label>
+                <input type="file" class="form-control" id="screenshot" name="screenshot" />
+            </div>
+
             <button type="submit" class="btn btn-primary">Envoyer</button>
         </form>
     </div>
