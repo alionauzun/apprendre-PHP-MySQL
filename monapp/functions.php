8f3c6cacd9f4ad6056f14338d858b090748a8429
Aliona <aliona-balti@outlook.fr>
2024-02-10 10:37:11 +0100

    ajout de la fonction redirectToUrl, pour rediriger l'utilisateur vers la page d'accueil

--- monapp/functions.php
+++ monapp/functions.php
@@ -1,6 +1,5 @@
 <?php
-
-//fonction qui affiche l'auteur de la recette en fonction de son email et de la liste des utilisateurs 
+// la fonction displayAuthor retourne une chaîne de caractères contenant le nom de l'auteur de la recette, son âge et le nombre de recettes qu'il a publiées. 
 function displayAuthor(string $authorEmail, array $users): string
 {
     foreach ($users as $user) {
@@ -8,10 +7,11 @@ function displayAuthor(string $authorEmail, array $users): string
             return $user['full_name'] . '(' . $user['age'] . ' ans)';
         }
     }
+
     return 'Auteur inconnu';
 }
 
-//fonction qui vérifie si une recette est valide
+// la fonction isValidRecipe retourne un booléen en fonction de la validité d'une recette donnée en paramètre 
 function isValidRecipe(array $recipe): bool
 {
     if (array_key_exists('is_enabled', $recipe)) {
@@ -20,18 +20,26 @@ function isValidRecipe(array $recipe): bool
         $isEnabled = false;
     }
 
-
     return $isEnabled;
 }
 
-//fonction qui retourne les recettes valides
+// la fonction getRecipes retourne un tableau contenant les recettes valides d'un tableau de recettes donné en paramètre 
 function getRecipes(array $recipes): array
 {
     $valid_recipes = [];
+
     foreach ($recipes as $recipe) {
         if (isValidRecipe($recipe)) {
             $valid_recipes[] = $recipe;
         }
     }
+
     return $valid_recipes;
 }
+
+// la fonction redirectToUrl redirige l'utilisateur vers une autre page, dans ce cas, la page d'accueil du site, qui est située à l'URL index.php. 
+function redirectToUrl(string $url): never
+{
+    header("Location: {$url}");
+    exit();
+}//exit()  est utilisé pour arrêter immédiatement le reste du code PHP. Cela garantit que la redirection s'effectue sans problème, sans que d'autres instructions perturbent ce processus.
